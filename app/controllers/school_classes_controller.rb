18a3791a68418852c092914958f6a505459f8d24
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-23 23:41:06 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,6 +6,8 @@ class SchoolClassesController < ApplicationController
 
 
   def create
+    @school_class = SchoolClass.new(title: params[:title], room_number: params[:room_number])
+    @school_class.save
   end
 
   def edit
