4c086bb62cc912a68804c6e33247e3111a666de5
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-23 23:13:03 +0000

    automatically backed up by learn

--- db/migrate/20180923230952_create_school_classes.rb
+++ db/migrate/20180923230952_create_school_classes.rb
@@ -1,6 +1,7 @@
 class CreateSchoolClasses < ActiveRecord::Migration
   def change
-    create_table :school_classes do |t|
+    add_column :school_classes, :title, :string
+    add_column :school_classes, :room_number, :integer 
     end
   end
 end
