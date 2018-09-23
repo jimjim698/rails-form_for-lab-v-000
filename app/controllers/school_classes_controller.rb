8f6617ba89d75dce6e58e20bde3af7a2b285eb29
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-23 23:51:47 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,6 +6,9 @@ class SchoolClassesController < ApplicationController
     @school_class = SchoolClass.new
   end
 
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
 
   def create
     @school_class = SchoolClass.new(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
